554f508feda86a5207cd8b1d82901ff357479de1
tokcide <89933773+tokcide@users.noreply.github.com>
2023-08-23 17:33:31 +0530

    revamped projects

---
+++ packages/preset-eslint/index.eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: [
+    "turbo",
+    "prettier",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:astro/recommended",
+    "plugin:astro/jsx-a11y-recommended",
+    "plugin:solid/recommended",
+  ],
+  overrides: [
+    {
+      // Define the configuration for `.astro` file.
+      files: ["*.astro"],
+      // Allows Astro components to be parsed.
+      parser: "astro-eslint-parser",
+      // Parse the script in `.astro` as TypeScript by adding the following configuration.
+      // It's the setting you need when using TypeScript.
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {
+        // override/add rules settings here, such as:
+        // "astro/no-set-html-directive": "error"
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "solid"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": 0,
+  },
+};
---
+++ packages/preset-prettier/index.prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: [
+      "prettier-plugin-astro",
+      "prettier-plugin-tailwindcss",
+      "prettier-plugin-toml"
+    ],
+    printWidth: 120,
+    tabWidth: 2,
+    useTabs: false,
+    semi: true,
+    singleQuote: false,
+    trailingComma: "es5",
+    bracketSpacing: true,
+    bracketSameLine: false,
+    arrowParens: "always"
+};
